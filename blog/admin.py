68bd99d3160e986b04dc4ac7ed5411dbe76a88c6
BelieveElikem <123392549+believeelikem@users.noreply.github.com>
2025-12-01 13:56:33 +0000

    final touches

--- blog/admin.py
+++ blog/admin.py
@@ -1,22 +1,38 @@
 from tkinter.tix import Tree
 from django.contrib import admin
 from .models import Book, Post
-
+from django.contrib import messages
 
 
 class TestAdminPermissions(admin.ModelAdmin):
     
-    # def has_add_permission(self, request):
-    #     return False
+    def has_add_permission(self, request):
+        return True
     
     def has_change_permission(self, request, obj = None):
         # if obj is None:
         #     return True
-        return False
+        return True
     
-    # def has_delete_permission(self, request, obj =  None):
-    #     if obj is None:
-    #         return False
+    def has_delete_permission(self, request, obj =  None):
+        # if obj is None:
+        #     return False
+        # return True
+        # if obj is None:
+        #     return False
+        # return obj.pk != 5
+        
+        
+        ''' note the below doesnt check if the users group has a permission. it is only 
+         checking if the user is in a specific group
+        
+        if request.user.groups.filter(name = "Editors").exists():    
+             return True  '''
+    
+        # if request.POST.get("action") == "delete_selected":
+        #     messages.info(request, " Deleting action happening")
+        
+        return True
     
     # def has_view_permission(self, request, obj = ...):
     #     return False
