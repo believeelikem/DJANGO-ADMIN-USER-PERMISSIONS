47f6f8466a55c0da1295f0121687358bc5e6125f
BelieveElikem <123392549+believeelikem@users.noreply.github.com>
2025-12-01 11:27:03 +0000

    first commit

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from turtle import mode, title
+from django.db import models
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=224)
+    
+    def __str__(self):
+        return self.title
